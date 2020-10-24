be9d4056d8b70850db961ea67e1a684c96064654
uraoz <44524215+uraoz@users.noreply.github.com>
2020-10-25 01:19:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,12 +13,11 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.event
-async def on_message(message):
-    if message.content.startswith("/ping"):
-        await message.channel.send("pong")
 
 @bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
 async def dice(ctx, arg):
     await ctx.send(arg)
 
