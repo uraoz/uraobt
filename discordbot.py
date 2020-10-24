72e153ae43de3e1297946b8b757e042a08168caa
uraoz <44524215+uraoz@users.noreply.github.com>
2020-10-25 01:01:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import os
+import random
 import traceback
 
 bot = commands.Bot(command_prefix='/')
@@ -12,10 +13,9 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+@bot.event
+async def on_message(message):
+    if message.content.startswith("ping"):
+        await message.channel.send("pong")
 
 bot.run(token)
