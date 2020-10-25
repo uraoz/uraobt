e4a5d03f3e1400dc7fee918dc0ff70317fa00b4f
uraoz <uraot2@gmail.com>
2020-10-25 20:29:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -36,7 +36,10 @@ async def shuffle(ctx, arg):
 @bot.command()
 async def shufflist(ctx, *args):
     random.shuffle(args)
-    await ctx.send('{}'.format(' '.join(str(x) for x in args)))
+    mes=""
+    for i in range(len(args)):
+        mes=mes+args[i]
+    await ctx.send(mes)
 
 @bot.command()
 async def choice(ctx, *args):
