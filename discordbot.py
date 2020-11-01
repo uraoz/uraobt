788c45970a59ff42363dce9e72b13a16dadc95d5
uraoz <uraot2@gmail.com>
2020-11-01 18:04:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,29 @@
 from discord.ext import commands
 import os
 import discord
+import asyncio
 import random
 import traceback
 import time
+import youtube_dl
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -93,7 +112,7 @@ async def voicefile(ctx):
 @bot.command()
 async def voiceurl(ctx):
     await ctx.send("test機能")
-    
+
 
 @bot.command()
 async def voiceexit(ctx):
