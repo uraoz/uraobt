04c5de54a2be7c4ab3998c0987aa793514124c0d
uraoz <uraot2@gmail.com>
2021-03-07 14:36:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -199,7 +199,7 @@ async def voiceurl(ctx,*args):
             isCreated = False
             msg = None
             while fla < 7300:
-                fla = fla + 28
+                fla = fla + 24
                 img = Image.open(f"frames/frame{fla}.jpg")
                 frame = generate_frame(img,60)
                 if frame != None:
@@ -208,7 +208,7 @@ async def voiceurl(ctx,*args):
                         isCreated = True
                     else:
                         await msg.edit(content=frame)
-                        await asyncio.sleep(1)
+                        await asyncio.sleep(0.96)
                         
     except:
         await ctx.send("すでに再生中")
