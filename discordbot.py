ea5b40bdd9c0d03c843322298aceb4f06d7ba21f
uraoz <44524215+uraoz@users.noreply.github.com>
2021-09-19 05:18:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -57,6 +57,7 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send("```"+error_msg+"```")
+    print(error)
     print(error_msg)
 
 @bot.command()
